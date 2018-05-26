6a6a5fc77d63fec828f0ec7ed3add0a1cbdcd3db
antibioticvz@gmail.com <antibioticvz@gmail.com>
2018-05-26 16:56:14 +0300

    redis port fix

--- src/testSetup/setup.ts
+++ src/testSetup/setup.ts
@@ -3,7 +3,10 @@ import { startServer } from "../startServer";
 export const setup = async () => {
   const app = await startServer();
   const { port } = app.address();
-  /* console.log("...........................>>>>>>");
-  console.log(app.address().port); */
+
+  // console.log("...........................server>>>>>>");
+  // console.log(app.address());
+  // console.log(port);
   process.env.TEST_HOST = `http://127.0.0.1:${port}`;
+  // process.env.TEST_HOST = `http://127.0.0.1:$6262`;
 };
