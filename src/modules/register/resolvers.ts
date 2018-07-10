88d9bc3af3abae311d115d0941b1b0f8d2757413
antibioticvz@gmail.com <antibioticvz@gmail.com>
2018-07-10 20:53:35 +0300

    small fiix

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,15 +1,16 @@
-import * as bcrypt from "bcryptjs";
-import * as yup from "yup";
-import { ResolverMap } from "../../types/graphql-utils";
-import { User } from "../../entity/User";
-import { formatYupError } from "../../utils/formatYupError";
-import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink";
+import * as bcrypt from "bcryptjs"
+import * as yup from "yup"
+
+import { ResolverMap } from "../../types/graphql-utils"
+import { User } from "../../entity/User"
+import { formatYupError } from "../../utils/formatYupError"
+import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink"
 import {
   emailNotLongEnough,
   invalidEmail,
   passwordNotLongEnough,
   duplicateEmail
-} from "./errorMessages";
+} from "./errorMessages"
 
 const schema = yup.object().shape({
   email: yup
@@ -21,29 +22,25 @@ const schema = yup.object().shape({
     .string()
     .min(5, passwordNotLongEnough)
     .max(255)
-});
+})
 
 export const resolvers: ResolverMap = {
   Query: {
     bye: () => "bye User"
   },
   Mutation: {
-    register: async (
-      _,
-      args: GQL.IRegisterOnMutationArguments,
-      { redis, url }
-    ) => {
+    register: async (_, args: GQL.IRegisterOnMutationArguments, { redis, url }) => {
       try {
-        await schema.validate(args, { abortEarly: false });
+        await schema.validate(args, { abortEarly: false })
       } catch (err) {
-        return formatYupError(err);
+        return formatYupError(err)
       }
-      const { email, password } = args;
+      const { email, password } = args
 
       const userAlreadyExist = await User.findOne({
         where: { email },
         select: ["id"]
-      });
+      })
 
       if (userAlreadyExist) {
         return [
@@ -51,21 +48,21 @@ export const resolvers: ResolverMap = {
             path: "email",
             message: duplicateEmail
           }
-        ];
+        ]
       }
 
-      const hashedPassword = await bcrypt.hash(password, 10);
+      const hashedPassword = await bcrypt.hash(password, 10)
       const user = User.create({
         email,
         password: hashedPassword
-      });
+      })
 
-      await user.save();
+      await user.save()
 
       // ToDo implement an email client to sent a User register link
-      await createConfirmEmailLink(url, user.id, redis);
+      await createConfirmEmailLink(url, user.id, redis)
 
-      return null;
+      return null
     }
   }
-};
+}
--- src/utils/createConfirmEmailLink.test.ts
+++ src/utils/createConfirmEmailLink.test.ts
@@ -1,39 +1,35 @@
-import * as Redis from "ioredis";
+import * as Redis from "ioredis"
 
-import { createConfirmEmailLink } from "./createConfirmEmailLink";
-import { createTypeormConn } from "./createTypeormConn";
-import { User } from "../entity/User";
-import fetch from "node-fetch";
+import { createConfirmEmailLink } from "./createConfirmEmailLink"
+import { createTypeormConn } from "./createTypeormConn"
+import { User } from "../entity/User"
+import fetch from "node-fetch"
 
-const redis = new Redis();
-let userId = "";
+const redis = new Redis()
+let userId = ""
 
 beforeAll(async () => {
-  await createTypeormConn();
+  await createTypeormConn()
   const user = await User.create({
     email: "nnn@nnn.nn",
     password: "dfdfdfdfdfdf"
-  }).save();
-  userId = user.id;
-});
+  }).save()
+  userId = user.id
+})
 
 describe("createConfirmEmailLink", async () => {
   it("Success: User was created and is deleted from redis", async () => {
-    const url = await createConfirmEmailLink(
-      process.env.TEST_HOST as string,
-      userId,
-      redis
-    );
+    const url = await createConfirmEmailLink(process.env.TEST_HOST as string, userId, redis)
 
-    const response = await fetch(url);
-    const text = await response.text();
-    expect(text).toEqual("ok");
-    const user = await User.findOne({ where: { id: userId } });
-    expect((user as User).confirmed).toBeTruthy();
-    // be sure that we remowe the redis id
-    const chunks = url.split("/");
-    const key = chunks[chunks.length - 1];
-    const keyVal = await redis.get(key);
-    expect(keyVal).toBeNull();
-  });
-});
+    const response = await fetch(url)
+    const text = await response.text()
+    expect(text).toEqual("ok")
+    const user = await User.findOne({ where: { id: userId } })
+    expect((user as User).confirmed).toBeTruthy()
+    // be sure that we remove the redis id
+    const chunks = url.split("/")
+    const key = chunks[chunks.length - 1]
+    const keyVal = await redis.get(key)
+    expect(keyVal).toBeNull()
+  })
+})
