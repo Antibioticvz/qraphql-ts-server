9d443039c9f1ec0ea645c8539e802670a7d8af34
antibioticvz@gmail.com <antibioticvz@gmail.com>
2018-07-12 22:50:06 +0300

    add me test

--- src/modules/me/me.test.ts
+++ src/modules/me/me.test.ts
@@ -1,28 +1,43 @@
 import axios from "axios"
 import * as Chance from "chance"
 import { User } from "../../entity/User"
-
 import { createTypeormConn } from "../../utils/createTypeormConn"
 import { Connection } from "typeorm"
-import async from "./middleware"
 
 const chance = new Chance()
 
+let userId: string
+let connection: Connection
 const email = chance.email({ domain: "gmail.com", length: 10 })
 const password = chance.string({ length: 10 })
 
-const mutation = (e: string, p: string) => `
+const loginMutation = (e: string, p: string) => `
 mutation {
-    register(email: "${e}", password: "${p}"){
-      path
-      message
-    }
+  login(email: "${e}", password: "${p}") {
+    path
+    message
+  }
 }
 `
 
-let connection: Connection
+const meQuery = `
+{
+  me {
+    id
+    email
+  }
+}
+`
+
 beforeAll(async () => {
   connection = await createTypeormConn()
+
+  const user = await User.create({
+    email,
+    password,
+    confirmed: true
+  }).save()
+  userId = user.id
 })
 afterAll(async () => {
   connection.close()
@@ -33,70 +48,32 @@ describe("Me from cooki", async () => {
     // later
   })
 
-  it("", async () => {
-    // later
-  })
-})
-
-describe("Register User", async () => {
-  it("c make sure we can register a user", async () => {
-    const res = await request(process.env.TEST_HOST as string, mutation(email, password))
-    expect(res).toEqual({ register: null })
-    const users = await User.find({ where: { email } })
-    expect(users).toHaveLength(1)
-    const user = users[0]
-    expect(user.email).toEqual(email)
-    expect(user.password).not.toEqual(password)
-  })
-
-  it("Reject: test for duplicate emails", async () => {
-    const res = await request(process.env.TEST_HOST as string, mutation(email, password))
-    expect(res.register).toHaveLength(1)
-    expect(res.register[0]).toEqual({
-      path: "email",
-      message: duplicateEmail
-    })
-  })
-
-  it("Reject: bad emails without @", async () => {
-    const res = await request(process.env.TEST_HOST as string, mutation(justString, password))
-    expect(res.register[0]).toEqual({
-      path: "email",
-      message: invalidEmail
-    })
-  })
-
-  it("Reject: Emails too short", async () => {
-    const res = await request(process.env.TEST_HOST as string, mutation(emailToShort, password))
-    expect(res.register[0]).toEqual({
-      path: "email",
-      message: emailNotLongEnough
-    })
-  })
-
-  it("Reject: Emails too short", async () => {
-    const res = await request(process.env.TEST_HOST as string, mutation(email, passwordToShort))
-    expect(res.register[0]).toEqual({
-      path: "password",
-      message: passwordNotLongEnough
-    })
-  })
-
-  it("Reject: Bad password and bad email", async () => {
-    const res = await request(
+  it("Success", async () => {
+    await axios.post(
       process.env.TEST_HOST as string,
-      mutation(justShortString, passwordToShort)
-    )
-    expect(res.register).toEqual([
       {
-        path: "email",
-        message: "email must be at least 7 characters"
+        query: loginMutation(email, password)
       },
-      { path: "email", message: "email must be a valid email" },
       {
-        path: "password",
-        message: "password must be at least 5 characters"
+        withCredentials: true
       }
-    ])
+    )
+
+    const response = await axios.post(
+      process.env.TEST_HOST as string,
+      {
+        query: meQuery
+      },
+      {
+        withCredentials: true
+      }
+    )
+
+    expect(response.data.data).toEqual({
+      me: {
+        id: userId,
+        email
+      }
+    })
   })
 })
