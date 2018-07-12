8e0582bd4d1051e01c4018548c64e8fc9e86c667
antibioticvz@gmail.com <antibioticvz@gmail.com>
2018-07-12 22:47:55 +0300

    cors origin test

--- src/startServer.ts
+++ src/startServer.ts
@@ -39,7 +39,7 @@ export const startServer = async () => {
   // my frontend
   const cors = {
     credentials: true,
-    origin: "http://localhost:3000"
+    origin: process.env.NODE_ENV === "test" ? "*" : "http://localhost:3000"
   }
 
   server.express.get("/confirm/:id", confirmEmail)
