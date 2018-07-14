2688ee751b58a9ea8c0c4e97800d1a3274e7050e
antibioticvz@gmail.com <antibioticvz@gmail.com>
2018-07-14 07:40:00 +0300

    logger to midleware

--- src/modules/me/middleware.ts
+++ src/modules/me/middleware.ts
@@ -1,7 +1,11 @@
 import { Resolver } from "../../types/graphql-utils"
+import logger from "../../utils/logger"
 
 export default async (resolver: Resolver, parent: any, args: any, context: any, info: any) => {
   // middleware
+  // logger(parent, args, context, info)
+  // end of middleware
+
   const result = await resolver(parent, args, context, info)
   // afterware
 
---
+++ src/utils/logger.ts
@@ -0,0 +1,8 @@
+const logger = (parent: any, args: any, context: any, info: any) => {
+  console.log(parent)
+  console.log(args)
+  console.log(context)
+  console.log(info)
+}
+
+export default logger
